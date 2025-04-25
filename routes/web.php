c328c371814a48c3f2dbf6320a27b70edc0eea2c
agngrm <agung.marlis17@gmail.com>
2025-04-25 19:08:41 +0700

    template awal

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+use App\Http\Controllers\DataKaryawan\DivisiController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/* Login, Home, Profile */
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/profile', 'ProfileController@index')->name('profile');
+Route::put('/profile', 'ProfileController@update')->name('profile.update');
+
+
+/* Data Karyawan */
+Route::resource('data-karyawan', DivisiController::class)
+    ->parameters(['data-karyawan' => 'ts_div']);
