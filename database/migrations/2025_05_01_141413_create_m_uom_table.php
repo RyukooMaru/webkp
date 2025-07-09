d308425b86844892a4718d66b8543654586793bc
agngrm <agung.marlis17@gmail.com>
2025-07-09 15:08:07 +0700

    fix migration

--- database/migrations/2025_05_01_141413_create_m_uom_table.php
+++ database/migrations/2025_05_01_141413_create_m_uom_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
 {
     Schema::create('m_uom', function (Blueprint $table) {
         // Change to bigIncrements to match Laravel's default ID type
-        $table->bigIncrements('UOM_Auto');
+        $table->id('UOM_Auto');
         $table->string('UOM_Code', 5)->unique();
         $table->decimal('UOM_Amount', 18, 2)->default(0.00);
         $table->string('UOM_EntryID', 10)->nullable();
@@ -18,7 +18,7 @@ return new class extends Migration {
         $table->timestamp('UOM_LastUpdate')->useCurrent();
     });
 }
-    
+
     public function down()
     {
         Schema::dropIfExists('m_uom');
